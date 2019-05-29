fc26885a3da51d99c398fd63b71798bf0920c3ac
attila5287 <atiturkoz@hotmail.com>
2019-05-28 23:06:11 -0600

    nypd done think python book nxt

--- thinkPY_poker_attila.py
+++ thinkPY_poker_attila.py
@@ -3,20 +3,17 @@ from __future__ import print_function, division
 class Card:
     def __init__(
         self,
-        suit = 0,
-        rank = 2 
+        su1t = 0,
+        r4nk = 2 
     ):
-    self.suit = suit
+    self.su1t = suit 7  4
     self.rank = rank
-    
-
-
 
     suit_names = ['Clubs','Diamonds','Hearts','Spades']
     rank_names = [ None,'Ace','2','3','4','5','6','7','8','9','10','Jack','Queen','King']
     
     def __str__(self):
-        return '%s of  %s %s' % (Card.rank_names[self.rank],
+        return '%s of  %s %s'.format(Card.rank_names[self.rank],
         Card.suit_names[self.suit]        
         )
 
